872b3c975a679dfe9b1d9e2281121c17232112fc
kingpascal <34338394+kingpascal@users.noreply.github.com>
2022-08-05 14:57:49 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,59 +1,30 @@
 #include "function_pointers.h"
 
-
-
 /**
- *
- *   * int_index - ...
- *
- *     * @array: ...
- *
- *       * @size: ...
- *
- *         * @cmp: ...
- *
- *           *
- *
- *             * Return: ...
- *
- *               */
-
+  * int_index - ...
+  * @array: ...
+  * @size: ...
+  * @cmp: ...
+  *
+  * Return: ...
+  */
 int int_index(int *array, int size, int (*cmp)(int))
-
 {
+	int i = 0;
 
-		int i = 0;
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
 
+				i++;
+			}
+		}
+	}
 
-
-			if (size > 0)
-
-					{
-
-								if (array != NULL && cmp != NULL)
-
-											{
-
-															while (i < size)
-
-																			{
-
-																								if (cmp(array[i]))
-
-																														return (i);
-
-
-
-																												i++;
-
-																															}
-
-																	}
-
-									}
-
-
-
-				return (-1);
-
-}`
+	return (-1);
+}
